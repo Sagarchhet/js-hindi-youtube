965a25f3b290499ea59c71bfb03b98359a2e8a07
Sagarchhet <146544413+Sagarchhet@users.noreply.github.com>
2023-10-02 09:32:29 +0000

    numbers and math done

---
+++ first_class/numbersmath.js
@@ -0,0 +1,29 @@
+// // // const score = 400
+// // // console.log(score);
+// // // const balance = new Number(100);
+// // // console.log(balance) //
+// // // console.log(balance.toString());
+
+// // const balance = new Number(700.11)
+// // // console.log(balance.toString().length);
+// // console.log(balance.toFixed(2));
+// // const otherNumber = 1235.8966
+// // console.log(otherNumber.toPrecision(6));
+// // const hundreds = 1000000
+// // console.log(hundreds.toLocaleString('en-IN'));
+// // math//
+// // console.log(Math.abs(-4));
+// // console.log(Math.round(4.7));
+// // console.log(Math.ceil(4.2));
+// // console.log(Math.floor(5.6));
+// // console.log(Math.max(4,3,9,8));
+// // console.log(Math.min(8,9,7,5,6));
+
+// // console.log(Math.random());
+// // console.log(Math.random()*10+1);
+// // console.log(Math.floor(Math.random()*10)+1);
+// const min = 10
+// const max = 20
+// // console.log(Math.random() *(max - min +1));
+// // console.log(Math.floor(Math.random() *(max - min +1)));
+// console.log(Math.ceil(Math.random() *(max - min +1)));
