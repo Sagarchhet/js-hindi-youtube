2cdf1188acc55fb1fe748981d7f1a8799deab78d
Sagarchhet <146544413+Sagarchhet@users.noreply.github.com>
2023-10-02 09:02:34 +0000

    datatypes

--- first_class/datatypes.js
+++ first_class/datatypes.js
@@ -1,3 +1,29 @@
-console.log("sagar");
+// const name = "sagar"
+// const repoCount = 50
+// console.log( name)
+// console.log(repoCount);
+// console.log('hello my name is $ { name } and my repo count is $ { repoCount }');
 
-console.log(3 + 3)
+// const gameName = new String('sagarchh')
+// // console.log(gameName[0]);//
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('a'));
+
+// const gameName = new String('sagarch')
+// const newString = gameName.substring(0, 5);
+// console.log(newString);
+// const anotherString = gameName.slice(-6, 0)
+// console.log(anotherString);
+
+// const newStringOne = "  Sagar  "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+// const url= "https://hitesth.com/hitesh%20choudhary"
+// console.log(url);
+// console.log(url.replace('%20', '_'));
+// console.log(url.replace('hitesh', 'sagar'));
+// console.log(url.replace('choudhary', 'chhetri'));
+// console.log(url.includes('sagar'));
