10ed95cb7fcf5919f356216b6ce0a7bf6d4f8855
Sagarchhet <146544413+Sagarchhet@users.noreply.github.com>
2023-10-02 10:04:43 +0000

    date done

---
+++ first_class/date.js
@@ -0,0 +1,26 @@
+//  let myDate = new Date()
+// console.log(myDate)
+//  console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString);
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023,0,23)
+// console.log(myCreatedDate)
+// console.log(myCreatedDate.toDateString);
+
+// let myCreatedDate = new Date (1999,3,30, 5, 3);
+// console.log(myCreatedDate.toDateString())
+// let myCreatedDate = new Date("05-31-1999");
+// console.log(myCreatedDate.toDateString());
+// let myTimeSpamp = Date.now()
+// console.log(myTimeSpamp);
+// let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+// let newDate = new Date()
+// console.log(newDate)
+// console.log(newDate.getMonth());
+// console.log(newDate.getDay());
+
