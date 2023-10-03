fef425d40f73948a1b496e4a660e856e41c3dc8f
Sagarchhet <146544413+Sagarchhet@users.noreply.github.com>
2023-10-03 12:20:24 +0000

    arey done

---
+++ first_class/array.js
@@ -0,0 +1,46 @@
+const myArr = [1, 2, 3, 4, 5, "sagar"]
+// console.log(myArr[4]);
+
+// const myHeros = ["shaktiman", "spiderman", "hatim"]
+// console.log(myHeros);
+
+// myArr.push(7)
+// myArr.pop()
+// console.log(myArr)
+// myArr.unshift(8)
+// console.log(myArr)
+// myArr.shift()
+// console.log(myArr)
+// console.log(myArr.includes("sagar"));
+// console.log(myArr.indexOf(4));
+// const newArr = myArr.join()
+// console.log(newArr);
+// console.log("A ", myArr);
+// const myn1 = myArr.slice(1.4)
+// console.log(myn1);
+// console.log("B ", myArr);
+// const myn2 = myArr.splice(1, 3)
+// console.log("C ", myArr);
+// const marvel_heros = ["thor", "Ironman", "captain"]
+// const indian_heros = ["shaktiman", "hatim", "Sagar"]
+// marvel_heros.push(indian_heros)
+// console.log(marvel_heros);
+// console.log(marvel_heros[3] [1]);
+// const allHeros = marvel_heros.concat(indian_heros)
+// console.log(allHeros);
+
+// const all_newheros = [...marvel_heros, ...indian_heros]
+// console.log(all_newheros);
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7,[4, 5]]]
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+console.log(Array.isArray("sagar"));
+console.log(Array.from("hitesh"))
+// console.log(Array.from({{naem: "hitesh"}}))
+
+
+let score = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of (score, score2, score3));
